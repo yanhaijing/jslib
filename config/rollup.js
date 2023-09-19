e89161767b4b3af0e0f09ed5b74f26c2647aba3e
yanhaijing <yanhaijing@yeah.net>
2023-09-20 00:02:16 +0800

    feat: init

---
+++ config/rollup.js
@@ -0,0 +1,48 @@
+var babel = require('@rollup/plugin-babel');
+
+var pkg = require('../package.json');
+
+var version = pkg.version;
+
+var banner = `/*!
+ * ${pkg.name} ${version} (https://github.com/yanhaijing/jslib)
+ * API https://github.com/yanhaijing/jslib/blob/master/doc/api.md
+ * Copyright 2017-${new Date().getFullYear()} yanhaijing. All Rights Reserved
+ * Licensed under MIT (https://github.com/yanhaijing/jslib/blob/master/LICENSE)
+ */
+`;
+
+function getCompiler() {
+  return babel({
+    babelrc: false,
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            browsers:
+              'last 2 versions, > 1%, ie >= 11, Android >= 4.1, iOS >= 10.3',
+            node: '14',
+          },
+          modules: false,
+          loose: false,
+        },
+      ],
+    ],
+    plugins: [
+      [
+        '@babel/plugin-transform-runtime',
+        {
+          helpers: true,
+          regenerator: false,
+        },
+      ],
+    ],
+    babelHelpers: 'runtime',
+    exclude: 'node_modules/**',
+  });
+}
+
+exports.name = 'jslib';
+exports.banner = banner;
+exports.getCompiler = getCompiler;
